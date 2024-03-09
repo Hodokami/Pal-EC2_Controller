9f99dcc29d68a464f0d86649a3444d4239b7a986
Hodokami <40252783+Hodokami@users.noreply.github.com>
2024-03-10 05:37:26 +0900

    Make Repository

---
+++ rconauth-sample.php
@@ -0,0 +1,5 @@
+<?php
+$host = 'palserver.example.com'; // Server host name or IP
+$port = 25575;                      // Port rcon is listening on
+$password = 'password'; // rcon.password setting set in server.properties
+$timeout = 5;                       // How long to timeout.
---
+++ www/listonly.php
@@ -0,0 +1,36 @@
+<?php
+require_once('palrcon.php');
+require_once('../../rconauth.php');
+use Thedudeguy\Rcon;
+$rcon = new Rcon($host, $port, $password, $timeout);
+if ($rcon->connect())
+{
+	$info = $rcon->sendCommand("info");
+	echo $info.'<br>';
+	$playersraw = $rcon->sendCommand("showplayers");
+	$players = array_chunk(preg_split("/[\s,]+/", htmlspecialchars( $playersraw, ENT_QUOTES)),3);
+	$rcon->disconnect();
+}
+else
+{
+	echo "Timeout.";
+	$rcon->disconnect();
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP RCON TEST</title>
+	</head>
+	<body>
+		Online Players:<br>
+		<?php
+			foreach($players as $key => $value)
+			{
+				if($key === 0){ continue; }
+				echo $value[0].'<br>';
+			}
+		?>
+	</body>
+</html>
