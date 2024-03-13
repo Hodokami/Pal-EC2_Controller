b1f8ae5b215a314dfc36eee4d58bc57398fa2d63
Hodokami <40252783+Hodokami@users.noreply.github.com>
2024-03-14 04:48:41 +0900

    add auth info sample

--- auth-sample.php
+++ auth-sample.php
@@ -1,5 +1,13 @@
 <?php
+// Palworld RCON auth settings
 $host = 'palserver.example.com'; // Server host name or IP
 $port = 25575;                      // Port rcon is listening on
-$password = 'password'; // rcon.password setting set in server.properties
+$password = 'password'; // adminpassword setting set in server.properties
 $timeout = 5;                       // How long to timeout.
+
+// AWS SDK
+$region = 'ap-northeast-1';
+$instanceIds = ['i-xxxxxxxxxxxxxxxxx'];
+
+// Discord Werhook URL
+$webhook = 'https://discord.com/api/webhooks/0000000000000000000/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
