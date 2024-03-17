b9d5762b8714e3f067c05dab38ab209c034d497b
Hodokami <40252783+Hodokami@users.noreply.github.com>
2024-03-18 05:11:50 +0900

    change REDIRECT for avoid cache.

--- www/index.php
+++ www/index.php
@@ -1,4 +1,15 @@
 <?php
+if(isset($_GET['cache'])) // Avoid KUSANAGI fcache
+{
+	if($_GET['cache'] !== 'false')
+	{
+		header('Location:'.$_SERVER['PHP_SELF'].'?cache=false');
+	}
+}
+else
+{
+	header('Location:'.$_SERVER['PHP_SELF'].'?cache=false');
+}
 session_start();
 
 require_once __DIR__.'/../auth.php';
@@ -25,60 +36,15 @@ function post2discord($post, $url)
 if(isset($_SESSION['loggedin'])) { if($_SESSION['loggedin'] === true) { $status = 'Logged in!'; } else { $status = 'Not logged in.'; } } else { $status = 'No status.'; }
 if(isset($_SESSION['timeunlock'])) { if($_SESSION['timeunlock'] <= time()) { $locked = false; } else { $locked = true; }} else { $locked = false; }
 
-if(array_key_exists('sendpw', $_POST))
+if($locked === false) // Get EC2 Instance State
 {
-	if($locked === false)
-	{
-		$_SESSION['timeunlock'] = time() + 10;
-		$_SESSION['password'] = bin2hex(random_bytes(12));
-		$message = json_encode(array('username' => 'Login Password', 'content' => $_SESSION['password']));
-		post2discord($message, $webhook);
-	}
-	header('Location:' . $_SERVER['PHP_SELF']);
+	$result = $ec2Client -> describeInstanceStatus(['IncludeAllInstances' => true, 'InstanceIds' => $instanceIds,]);
+	$state = $result['InstanceStatuses'][0]['InstanceState']['Name'];
+	if(!isset($state)){$state = 'locked';}
 }
-
-if(array_key_exists('login', $_POST))
+else
 {
-	$gotpw = htmlspecialchars(strip_tags($_POST['password']), ENT_QUOTES, 'UTF-8'); // XSS対策
-	if(isset($_SESSION['password']))
-	{
-		if($_SESSION['password'] === $gotpw)
-		{
-			$_SESSION['loggedin'] = true;
-		}
-		else
-		{
-			session_unset(); // passwordが間違った時点ですべてリセット
-		}
-		header('Location:' . $_SERVER['PHP_SELF']);
-	}
-	else
-	{
-		echo '<script>alert("Password is not sent!")</script>';
-		session_unset();
-	}
-}
-
-$result = $ec2Client -> describeInstanceStatus(['IncludeAllInstances' => true, 'InstanceIds' => $instanceIds,]);
-$state = $result['InstanceStatuses'][0]['InstanceState']['Name'];
-if(!isset($state)){echo 'AWS API Error.';}
-
-if(array_key_exists('start', $_POST))
-{
-	exec('php '.__DIR__.'/serverctrl.php start >/dev/null 2>&1 &');
-	header('Location:' . $_SERVER['PHP_SELF']);
-}
-
-if(array_key_exists('stop', $_POST))
-{
-	exec('php '.__DIR__.'/serverctrl.php stop >/dev/null 2>&1 &');
-	header('Location:' . $_SERVER['PHP_SELF']);
-}
-
-if(array_key_exists('frestart', $_POST))
-{
-	exec('php '.__DIR__.'/serverctrl.php frestart >/dev/null 2>&1 &');
-	header('Location:' . $_SERVER['PHP_SELF']);
+	$state = 'locked';
 }
 
 if($state === 'running' && $locked === false) // Get Players List
@@ -96,6 +62,83 @@ if($state === 'running' && $locked === false) // Get Players List
 		$rcon->disconnect();
 	}
 }
+
+if(array_key_exists('sendpw', $_POST))
+{
+	if($locked === false)
+	{
+		$_SESSION['timeunlock'] = time() + 10;
+		$_SESSION['password'] = bin2hex(random_bytes(12));
+		$message = json_encode(array('username' => 'Login Password', 'content' => $_SESSION['password']));
+		post2discord($message, $webhook);
+	}
+	header('Location:'.$_SERVER['PHP_SELF'].'?cache=false');
+}
+
+if(array_key_exists('login', $_POST))
+{
+	$gotpw = htmlspecialchars(strip_tags($_POST['password']), ENT_QUOTES, 'UTF-8'); // XSS対策
+	if(isset($_SESSION['password']))
+	{
+		if($_SESSION['password'] === $gotpw)
+		{
+			$_SESSION['timeunlock'] = time();
+			$_SESSION['loggedin'] = true;
+		}
+		else
+		{
+			session_unset(); // passwordが間違った時点ですべてリセット
+		}
+		header('Location:'.$_SERVER['PHP_SELF'].'?cache=false');
+	}
+	else
+	{
+		echo '<script>alert("Password is not sent!")</script>';
+		session_unset();
+	}
+}
+
+if(array_key_exists('start', $_POST))
+{
+	if(isset($_SESSION['loggedin']))
+	{
+		if($_SESSION['loggedin'] === true && $state === 'stopped')
+		{
+			$_SESSION['timeunlock'] = time() + 180;
+			$exec = 'php '.__DIR__.'/serverctrl.php start >/dev/null 2>&1 &';
+			exec($exec);
+		}
+	}
+	header('Location:'.$_SERVER['PHP_SELF'].'?cache=false');
+}
+
+if(array_key_exists('stop', $_POST))
+{
+	if(isset($_SESSION['loggedin']))
+	{
+		if($_SESSION['loggedin'] === true)
+		{
+			$_SESSION['timeunlock'] = time() + 180;
+			$exec = 'php '.__DIR__.'/serverctrl.php stop >/dev/null 2>&1 &';
+			exec($exec);
+		}
+	}
+	header('Location:'.$_SERVER['PHP_SELF'].'?cache=false');
+}
+
+if(array_key_exists('frestart', $_POST))
+{
+	if(isset($_SESSION['loggedin']))
+	{
+		if($_SESSION['loggedin'] === true)
+		{
+			$_SESSION['timeunlock'] = time() + 180;
+			$exec = 'php '.__DIR__.'/serverctrl.php frestart >/dev/null 2>&1 &';
+			exec($exec);
+		}
+	}
+	header('Location:'.$_SERVER['PHP_SELF'].'?cache=false');
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -105,6 +148,7 @@ if($state === 'running' && $locked === false) // Get Players List
 	</head>
 	<body>
 		<p>Your status: <?php echo $status;?></p>
+		<p>Instance status: <?php echo $state;?></p>
 		<form method = "POST">
 			<p><input type="submit" name="sendpw" class="button" value="Send password" <?php if($locked === true) { echo 'disabled'; }?>></p>
 		</form>
@@ -114,7 +158,7 @@ if($state === 'running' && $locked === false) // Get Players List
 		<p>
 			<form method = "POST"><input type="submit" name="start" class="button" value="Start Server" <?php if($state !== 'stopped' || $locked === true) { echo 'disabled'; }?>></form>
 			<form method = "POST"><input type="submit" name="stop" class="button" value="Stop Server" <?php if($state !== 'running' || $locked === true) { echo 'disabled'; }?>></form>
-			<form method = "POST"><input type="submit" name="frestart" class="button" value="&quot;FORCE&quot; Restart Server"></form>
+			<form method = "POST"><input type="submit" name="frestart" class="button" value="&quot;FORCE&quot; Restart Server" <?php if($locked === true) { echo 'disabled'; }?>></form>
 		</p>
 		<?php
 		if($state === 'running' && $locked === false){
