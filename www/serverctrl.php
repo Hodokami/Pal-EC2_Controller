c7c47a3c74376395f9204df9c783837d8961d2ee
Hodokami <40252783+Hodokami@users.noreply.github.com>
2024-04-09 16:35:22 +0900

    can change enviroments

--- www/serverctrl.php
+++ www/serverctrl.php
@@ -1,16 +1,20 @@
 <?php
-require_once __DIR__.'/../../auth.php'; // Params for RCON, AWS, and Discord.
+require_once __DIR__.'/../auth.php'; // Params for RCON, AWS, and Discord. FOR TEST ON Docker
+// require_once __DIR__.'/../../auth.php'; // Params for RCON, AWS, and Discord.
 // Composer
-require_once __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__.'/../../../vendor/autoload.php'; // FOR TEST ON Docker
+// require_once __DIR__.'/../../vendor/autoload.php'; // Settings for running on KUSANAGI
 // RCON Library by https://github.com/thedudeguy/PHP-Minecraft-Rcon
 use Hodokami\Rcon;
 $rcon = new Rcon($host, $port, $password, $timeout);
 // AWS SDK for PHP
-use Aws\Credentials\CredentialProvider;
-$provider = CredentialProvider::ini('default', __DIR__.'/../../credentials.ini');
-$provider = CredentialProvider::memoize($provider);
+// use Aws\Credentials\CredentialProvider;  // Credentials Settings for running on KUSANAGI
+// $provider = CredentialProvider::ini('default', __DIR__.'/../../credentials.ini');
+// $provider = CredentialProvider::memoize($provider);
 use Aws\Ec2\Ec2Client;
-$ec2Client = new Ec2Client(['region' => $region, 'version' => '2016-11-15', 'credentials' => $provider]);
+$ec2Client = new Ec2Client(['region' => $region, 'version' => '2016-11-15', 'profile' => 'default']);  // FOR TEST ON Docker
+// $ec2Client = new Ec2Client(['region' => $region, 'version' => '2016-11-15', 'credentials' => $provider]); // Settings for running on KUSANAGI
+
 if(isset($argv))
 {
 	if($argv[1] === 'start')
